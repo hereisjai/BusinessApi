b1c838f4654e64a900e9b31e013ca47fa6b51c36
jai <jk9693@gmail.com>
2024-04-26 15:39:07 +0530

    Addomg cors 5

--- src/main/java/com/business/sales/controller/CustomerController.java
+++ src/main/java/com/business/sales/controller/CustomerController.java
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.business.sales.entity.Customers;
 import com.business.sales.entity.repository.CustomerRepository;
 
 @RestController
+@CrossOrigin(origins ="*")
 public class CustomerController {
 
 	@Autowired
